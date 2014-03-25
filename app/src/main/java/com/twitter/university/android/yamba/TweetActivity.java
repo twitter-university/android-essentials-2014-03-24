abb25e66fce9ebc413a223506d88445af5097788
G. Blake Meike <bmeike@twitter.com>
2014-03-25 16:06:54 -0700

    Emulate AsyncTask networking

---
+++ app/src/main/java/com/twitter/university/android/yamba/TweetActivity.java
@@ -0,0 +1,122 @@
+package com.twitter.university.android.yamba;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+
+public class TweetActivity extends Activity {
+    private static final String TAG = "STATUS";
+
+    class Poster extends AsyncTask<String, Void, Integer> {
+        @Override
+        protected Integer doInBackground(String... strings) {
+
+            try { Thread.sleep(1000 * 10); }
+            catch (InterruptedException e) {}
+
+            return Integer.valueOf(R.string.tweet_success);
+        }
+
+        @Override
+        protected void onPostExecute(Integer msgId) {
+            Toast.makeText(TweetActivity.this, msgId.intValue(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    private int okColor;
+    private int warnColor;
+    private int errColor;
+
+    private int tweetLenMax;
+    private int warnMax;
+    private int errMax;
+
+    private Button submitButton;
+    private TextView countView;
+    private EditText tweetView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+        Log.d(TAG, "onCreate: " + this);
+
+        Resources rez = getResources();
+        okColor = rez.getColor(R.color.ok);
+        warnColor = rez.getColor(R.color.warn);
+        errColor = rez.getColor(R.color.error);
+
+        tweetLenMax = rez.getInteger(R.integer.tweet_limit);
+        warnMax = rez.getInteger(R.integer.warn_limit);
+        errMax = rez.getInteger(R.integer.err_limit);
+
+        submitButton = (Button) findViewById(R.id.tweet_submit);
+        submitButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) { sendTweet(); }
+                }
+        );
+
+        countView = (TextView) findViewById(R.id.tweet_count);
+
+        tweetView = ((EditText) findViewById(R.id.tweet_tweet));
+        tweetView.addTextChangedListener(
+            new TextWatcher() {
+                @Override
+                public void afterTextChanged(Editable editable) { setCount(); }
+
+                @Override
+                public void beforeTextChanged(CharSequence cs, int i, int i2, int i3) {  }
+
+                @Override
+                public void onTextChanged(CharSequence cs, int i, int i2, int i3) { }
+            }
+        );
+    }
+
+    void setCount() {
+        int n = tweetView.getText().length();
+
+        submitButton.setEnabled(checkTweetLen(n));
+
+        n = tweetLenMax - n;
+
+        int color;
+        if (n > warnMax) { color = okColor; }
+        else if (n > errMax) { color = warnColor; }
+        else  { color = errColor; }
+
+        countView.setText(String.valueOf(n));
+        countView.setTextColor(color);
+    }
+
+    void sendTweet() {
+        String tweet = tweetView.getText().toString();
+        if (!checkTweetLen(tweet.length())) { return; }
+
+        tweetView.setText("");
+
+        new Poster().execute(tweet);
+    }
+
+    private boolean checkTweetLen(int n) {
+        return (errMax < n) && (tweetLenMax > n);
+    }
+}
