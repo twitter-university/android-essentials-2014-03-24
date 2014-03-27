a4e833515aa62b95abb99d99351b337eda2b5552
G. Blake Meike <bmeike@twitter.com>
2014-03-27 15:43:48 -0700

    Final

--- app/src/main/java/com/twitter/university/android/yamba/TimelineActivity.java
+++ app/src/main/java/com/twitter/university/android/yamba/TimelineActivity.java
@@ -1,5 +1,6 @@
 package com.twitter.university.android.yamba;
 
+
 public class TimelineActivity extends YambaActivity {
     public TimelineActivity() { super("TIMELINE/A", R.layout.activity_timeline); }
 }
